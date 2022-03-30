86131901731bbb2308e50e4b8b8ba7793766461c
curder <q.curder@gmail.com>
2022-03-30 10:11:23 +0800

    Logging in

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterIndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
 Route::get('auth/register', RegisterIndexController::class)->name('register');
+Route::get('auth/login', LoginController::class)->name('login');
---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use function Pest\Laravel\post;
+
+uses(LazilyRefreshDatabase::class);
+
+it('redirects authenticated user', function() {
+    $user = User::factory()->create();
+
+    $this->actingAs($user)
+         ->get('/auth/login')
+         ->assertRedirect('/');
+});
+
+it('shows an errors if the details are not provided')
+    ->post('/login')
+    ->assertSessionHasErrors(['email', 'password']);
+
+it('logs the user in')->tap(function() {
+    $user = User::factory()->create();
+    post('/login', [
+        'email' => $user->email,
+        'password' => 'password',
+    ])->assertRedirect('/');
+
+})->assertAuthenticated();
