324b2a6e77372435345658947d887da8bada02db
curder <q.curder@gmail.com>
2022-03-30 22:06:25 +0800

    Add validation message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,11 +8,17 @@
         <div class="space-y-1">
             <label for="email" class="block">Email address</label>
             <input type="email" name="email" id="email" placeholder="e.g. you@example.com" class="rounded block w-full" />
+            @error('email')
+                <div class="text-xs text-red-600">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="space-y-1">
             <label for="password" class="block">Password</label>
             <input type="password" name="password" id="password" class="rounded block w-full" />
+            @error('password')
+                <div class="text-xs text-red-600">{{ $message }}</div>
+            @enderror
         </div>
 
         <button type="submit" class="bg-slate-200 hover:bg-slate-300 px-3 py-2 rounded">
--- resources/views/friends/partials/add_friend.blade.php
+++ resources/views/friends/partials/add_friend.blade.php
@@ -10,6 +10,9 @@
             <div class="space-y-1">
                 <label for="email" class="block">Email address</label>
                 <input type="email" name="email" id="email" placeholder="e.g. you@example.com" class="rounded block w-full" />
+                @error('email')
+                    <div class="text-xs text-red-600">{{ $message }}</div>
+                @enderror
             </div>
 
             <button type="submit" class="bg-slate-200 hover:bg-slate-300 px-3 py-2 rounded">
